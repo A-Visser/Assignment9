b0cab5af5201fb20167524e4fad1005c1f28131e
Aaron Visser <avisser20@stuy.edu>
2021-10-16 20:58:32 -0400

    changes

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+struct person { char name[10]; int height;};
+
+void print_person(struct person *p){
+  printf("name: %s\theight: %d cm\n", p->name, p->height);
+}
+struct person * make_person(char n[10], int h) {
+
+  struct person *m = malloc(sizeof(struct person));
+
+  strcpy(m->name, n);
+  m->height = h;
+  return m;
+}
+int main(){
+  struct person *y = make_person("Tim", 100);
+  print_person(y);
+  free(y);
+  struct person *t = make_person("Bill", (int)((double)100+((double)rand())/32767*100));
+  print_person(t);
+  free(t);
+  return 0;
+}
